3c9054cee3868b11e6e03495bc78726256d6d4d9
Joe Gain <joe.gain@mail.schwarz>
2023-03-19 17:27:46 +0100

    Initial commit

---
+++ src/main/java/io/gainable/sftpfilesequenzer/SftpFileSequenzerApplication.java
@@ -0,0 +1,13 @@
+package io.gainable.sftpfilesequenzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpFileSequenzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SftpFileSequenzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/gainable/sftpfilesequenzer/SftpFileSequenzerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gainable.sftpfilesequenzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftpFileSequenzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
